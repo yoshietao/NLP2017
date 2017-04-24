1ae24f6df93e0b63c8ca3ee38717775b4e4ebd09
yoshie <b02502108@ntu.edu.com.tw>
2017-04-24 21:31:31 +0800

    :)

--- hw1/test_opencc.py
+++
@@ -1,9 +0,0 @@
-#vim: set fileencoding:utf-8
-
-from opencc import OpenCC 
-
-openCC = OpenCC('s2t')  # convert from Simplified Chinese to Traditional Chinese
-# can also set conversion by calling set_conversion
-# openCC.set_conversion('s2tw')
-to_convert = u'开放中文转换'
-converted = openCC.convert(to_convert)
